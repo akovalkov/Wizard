7bb0dbe15cb2683dbdaca2d5901f0dee9442b66f
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 <akovalkov@yandex.ru>
2025-03-18 22:19:29 +0300

    The project tests

--- test/test-project.cpp
+++ test/test-project.cpp
@@ -8,35 +8,37 @@ using namespace Wizard;
 extern GlobalFixture fixture;
 
 TEST_CASE("Project load test") {
-    // auto projectFile = fixture.dataDir / "project.json";
-    // Project project;
-    // project.init(projectFile);
-    // CHECK(project.name == "WebProject");
-    // CHECK(project.description == "Test web project");
-    // CHECK(project.modules.size() == 1);
-    // CHECK(project.modules[0].name == "DatabaseSchema");
-    // std::vector<JsonTransformer::Filter> jtransform{{Template{""}, {{{"name", Template{"{{name}}"}, ""}}} },
-    //                                                 {Template{""}, {{{"host", Template{"{{host}}"}, ""}}} },
-    //                                                 {Template{"{{tables.id}}"}, {{{"tables", Template{"{{tables}}"}, "idtables"}}} },
-    //                                                 {Template{"{{not tables.id}}"}, {{{"tables", Template{"{{tables}}"}, ""}}} } };
-    // CHECK(project.modules[0].transformer.filters() == jtransform);
+    auto projectFile = fixture.dataDir / "project.json";
+    Project project;
+    project.init(projectFile);
+    CHECK(project.name == "WebProject");
+    CHECK(project.description == "Test web project");
+    CHECK(project.modules.size() == 1);
+    CHECK(project.modules[0].name == "DatabaseSchema");
+    std::vector<JsonTransformer::Rule> jtransform{{
+                                                    {"name", Template{}, Template{}, {}, {}},
+                                                    {"host", Template{}, Template{}, {}, {}},
+                                                    {"models", Template{"{{id}}"}, Template{}, "idtables", {}},
+                                                    {"models", Template{"{{not id}}"}, Template{}, "tables", {}}
+                                                  }};
+    CHECK(project.modules[0].transformer.rules() == jtransform);
 }
 
 TEST_CASE("Project transform test") {
-    // auto projectFile = fixture.dataDir / "project.json";
-    // auto dataFile = fixture.dataDir / "books.json";
-    // auto data = fixture.parse(dataFile);
-    // Project project;
-    // project.init(projectFile);
-    // auto newdata = project.modules[0].transformer.transform(data);
-    // CHECK(newdata.is_object());
-    // auto newdata_obj = newdata.as_object();
-    // CHECK(newdata_obj.if_contains("name"));
-    // CHECK(newdata_obj.at("name").is_string());
-    // CHECK(newdata_obj.if_contains("host"));
-    // CHECK(newdata_obj.at("host").is_string());
-    // CHECK(newdata_obj.if_contains("idtables"));
-    // CHECK(newdata_obj.at("idtables").is_array());
-    // CHECK(newdata_obj.if_contains("tables"));
-    // CHECK(newdata_obj.at("tables").is_array());
+    auto projectFile = fixture.dataDir / "project.json";
+    auto dataFile = fixture.dataDir / "books.json";
+    auto data = fixture.parse(dataFile);
+    Project project;
+    project.init(projectFile);
+    auto newdata = project.modules[0].transformer.transform(data);
+     CHECK(newdata.is_object());
+     auto newdata_obj = newdata.as_object();
+     CHECK(newdata_obj.if_contains("name"));
+     CHECK(newdata_obj.at("name").is_string());
+     CHECK(newdata_obj.if_contains("host"));
+     CHECK(newdata_obj.at("host").is_string());
+     CHECK(newdata_obj.if_contains("idtables"));
+     CHECK(newdata_obj.at("idtables").is_array());
+     CHECK(newdata_obj.if_contains("tables"));
+     CHECK(newdata_obj.at("tables").is_array());
 }
