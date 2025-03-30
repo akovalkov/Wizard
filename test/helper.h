fa4d1084cb2f04438e5a5252557616a4019b2b37
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 <akovalkov@yandex.ru>
2025-03-30 21:14:53 +0300

    Cosmetic changes

--- test/helper.h
+++ test/helper.h
@@ -35,50 +35,50 @@ struct GlobalFixture
 
 };
 
-static const char* enum_token_kind[] = {
-        "Token::Kind::Text",
-        "Token::Kind::ExpressionOpen",     // {{
-        "Token::Kind::ExpressionClose",    // }}
-        "Token::Kind::LineStatementOpen",  // ##
-        "Token::Kind::LineStatementClose", // \n
-        "Token::Kind::StatementOpen",      // {%
-        "Token::Kind::StatementClose",     // %}
-        "Token::Kind::CommentOpen",        // {#
-        "Token::Kind::CommentClose",       // #}
-        "Token::Kind::Id",                 // this, this.foo
-        "Token::Kind::Number",             // 1, 2, -1, 5.2, -5.3
-        "Token::Kind::String",             // "this"
-        "Token::Kind::Plus",               // +
-        "Token::Kind::Minus",              // -
-        "Token::Kind::Times",              // *
-        "Token::Kind::Slash",              // /
-        "Token::Kind::Percent",            // %
-        "Token::Kind::Power",              // ^
-        "Token::Kind::Comma",              // ,
-        "Token::Kind::Dot",                // .
-        "Token::Kind::Colon",              // :
-        "Token::Kind::LeftParen",          // (
-        "Token::Kind::RightParen",         // )
-        "Token::Kind::LeftBracket",        // [
-        "Token::Kind::RightBracket",       // ]
-        "Token::Kind::LeftBrace",          // {
-        "Token::Kind::RightBrace",         // }
-        "Token::Kind::Equal",               // ==
-        "Token::Kind::NotEqual",           // !=
-        "Token::Kind::GreaterThan",        // >
-        "Token::Kind::GreaterEqual",       // >=
-        "Token::Kind::LessThan",           // <
-        "Token::Kind::LessEqual",          // <=
-        "Token::Kind::Unknown",
-        "Token::Kind::Eof"
-};
+// static const char* enum_token_kind[] = {
+//         "Token::Kind::Text",
+//         "Token::Kind::ExpressionOpen",     // {{
+//         "Token::Kind::ExpressionClose",    // }}
+//         "Token::Kind::LineStatementOpen",  // ##
+//         "Token::Kind::LineStatementClose", // \n
+//         "Token::Kind::StatementOpen",      // {%
+//         "Token::Kind::StatementClose",     // %}
+//         "Token::Kind::CommentOpen",        // {#
+//         "Token::Kind::CommentClose",       // #}
+//         "Token::Kind::Id",                 // this, this.foo
+//         "Token::Kind::Number",             // 1, 2, -1, 5.2, -5.3
+//         "Token::Kind::String",             // "this"
+//         "Token::Kind::Plus",               // +
+//         "Token::Kind::Minus",              // -
+//         "Token::Kind::Times",              // *
+//         "Token::Kind::Slash",              // /
+//         "Token::Kind::Percent",            // %
+//         "Token::Kind::Power",              // ^
+//         "Token::Kind::Comma",              // ,
+//         "Token::Kind::Dot",                // .
+//         "Token::Kind::Colon",              // :
+//         "Token::Kind::LeftParen",          // (
+//         "Token::Kind::RightParen",         // )
+//         "Token::Kind::LeftBracket",        // [
+//         "Token::Kind::RightBracket",       // ]
+//         "Token::Kind::LeftBrace",          // {
+//         "Token::Kind::RightBrace",         // }
+//         "Token::Kind::Equal",               // ==
+//         "Token::Kind::NotEqual",           // !=
+//         "Token::Kind::GreaterThan",        // >
+//         "Token::Kind::GreaterEqual",       // >=
+//         "Token::Kind::LessThan",           // <
+//         "Token::Kind::LessEqual",          // <=
+//         "Token::Kind::Unknown",
+//         "Token::Kind::Eof"
+// };
 
 
-static const char* enum_states[] = {
-        "State::Text", 
-        "State::Statement", 
-        "State::LineStatement", 
-        "State::Expression", 
-        "State::Comment", 
-        "State::Eof"    
-};
+// static const char* enum_states[] = {
+//         "State::Text", 
+//         "State::Statement", 
+//         "State::LineStatement", 
+//         "State::Expression", 
+//         "State::Comment", 
+//         "State::Eof"    
+// };
